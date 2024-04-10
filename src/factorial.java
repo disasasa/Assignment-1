26f1dcfb012c434b309139f65b91c61d8c961cc4
Asignment 1 <230080@astanait.edu.kz>
2024-04-10 15:24:07 +0500

    Created fibonacci

---
+++ src/factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static int factorial(int n) {
+        if (n < 0) {
+            System.out.println("Error");
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            int result = 1;
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int factorialResult = factorial(number);
+        if (factorialResult != -1) {
+            System.out.println(factorialResult);
+        }
+        scanner.close();
+    }
+}
